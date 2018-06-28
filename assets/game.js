ab2a5f225d12da9f0d37b1354c03e7a6541fc474
arpitshukla114 <arpitshukla114@yahoo.co.in>
2018-06-28 18:31:55 -0400

    made some progress

--- assets/game.js
+++ assets/game.js
@@ -1,19 +1,19 @@
 $(document).ready(function(){
-    paintButtons();
-    var label = $('<p></p>');
-    label.text('Add a topic: ');
-    var input = $('<input type="text">');
-    var button = $('<input type="submit">');
-    inputForm.append(label);
-    inputForm.append(input);
-    inputForm.append(button);
-    $("#add-button").append(inputForm);
+    renderButtons();
+    renderRest();
+    $('#submit').on("click",function(){
+        var newVal = $('#topic').val();
+        topics.push(newVal);
+        renderButtons();
+
+    });
 });
 var topics = ['Tom Cruise','Brad Pitt','George Clooney','Kate Winslate','Maryl Streep','Margaret Robbie'];
 
-function paintButtons(){
+function renderButtons(){
+    $("#buttons").empty();
     var buttons = $('<div></div>');
-    var inputForm = $('<div></div>');
+    
     for(var i=0;i<topics.length;i++){
         var newButton = $('<button></button>');
         newButton.addClass("topic-button");
@@ -22,4 +22,16 @@ function paintButtons(){
         buttons.append(newButton);
     }
     $("#buttons").append(buttons);
+}
+
+function renderRest(){
+    var inputForm = $('<div></div>');
+    var label = $('<p></p>');
+    label.text('Add a topic: ');
+    var input = $('<input type="text" id="topic">');
+    var button = $('<input type="submit" id="submit">');
+    inputForm.append(label);
+    inputForm.append(input);
+    inputForm.append(button);
+    $("#add-button").append(inputForm);
 }
